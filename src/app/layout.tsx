0f3ed79badddfdf2ec0b26dfb4ef4159f66ae201
Nick Hong <me@nickhong.xyz>
2023-12-20 01:31:41 -0600

    Fix twitter share card image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { WagmiWrapper } from "./Wagmi";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Drift Vault",
   description: "Vault for FDO NFTs",
   metadataBase: new URL("https://vault.driftershoots.com"),
   openGraph: {
+    title: "Drift Vault",
+    description: "Vault for FDO NFTs",
     images: "/images/opengraph-image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Drift Vault",
+    description: "Vault for FDO NFTs",
+    creator: "@driftershoots",
+    creatorId: "413571172",
   },
 };
 
