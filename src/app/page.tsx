cc11b84b948d475be007f8e5bb31dc9982263a96
Nick Hong <me@nickhong.xyz>
2023-12-19 18:43:40 -0600

    Adjust overall page structure to stretch for larger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import { Vaulted } from "./Vault";
 
 export default function Home() {
   return (
-    <div className="max-h-screen overflow-auto">
-      <div className="hidden sm:block">
-        <Header />
+    <div className="max-h-screen min-h-screen h-full overflow-auto flex flex-col justify-between">
+      <div>
+        <div className="hidden sm:block">
+          <Header />
+        </div>
+        <div className="sm:hidden relative z-30">
+          <MobileHeader />
+        </div>
+        <main className="flex flex-col items-center justify-between px-1 sm:px-12 max-w-between-lg-xl mx-auto">
+          <Vaulted />
+        </main>
       </div>
-      <div className="sm:hidden relative z-30">
-        <MobileHeader />
-      </div>
-      <main className="flex flex-col items-center justify-between px-1 sm:px-12 max-w-between-lg-xl mx-auto">
-        <Vaulted />
-      </main>
-      <footer className="max-w-between-lg-xl mx-auto px-12 pb-6 text-slate-gray">
+      <footer className="max-w-between-lg-xl mx-auto px-12 pb-6 text-slate-gray w-full">
         <div>All materials © {new Date().getFullYear()} Isaac Wright</div>
         <div></div>
       </footer>
